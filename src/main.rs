983f84b8c920531cb9fba3c5e9df435835f3c0be
Harper Nordin <harper.nordin@gmail.com>
2024-11-01 10:20:25 -0700

    Opening a basic file and breaking it into lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::{File, read_to_string};
+use std::io::prelude::*;
+
+const STAFF_SIZE: usize = 11;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn get_file(file_path: String) -> Result<Vec<String>, &'static str> {
+    if let Ok(mut file) = File::open(file_path) {
+        let mut file_contents = String::new();
+
+        if let Ok(_) = file.read_to_string(&mut file_contents) {
+            return Ok(file_contents.lines().map(|str| str.to_string()).collect());
+        }
+
+        return Err("File could not be read to string");
+    }
+
+    Err("File could not be opened")
 }
