9f081aaba2040c0c35cb11119d9ef2eaf568a1e0
Harper Nordin <harper.nordin@gmail.com>
2024-11-04 21:02:18 -0800

    Added some types

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,65 @@
-use std::fs::File;
+use std::{fs::File, iter::Enumerate, str::Lines};
 use regex::Regex;
 use std::io::prelude::*;
+use thiserror::Error;
 
 const BASS_CENTER: &str = "D3";
 const TREBLE_CENTER: &str = "B5";
+
+#[derive(Error, Debug)]
+enum ParsingError {
+    #[error("Invalid Staff Identifier at line {0}")]
+    InvalidStaffDeclaration(usize),
+}
 
 enum StaffType {
     Treble,
     Bass,
 }
 
+enum Beats {
+    ThirtySecond,
+    Sixteenth,
+    Eighth,
+    Quarter,
+    Half,
+    Whole,
+    DottedThirtySecond,
+    DottedSixteenth,
+    DottedEighth,
+    DottedQuarter,
+    DottedHalf,
+    DottedWhole,
+    EighthTriplet,
+    QuarterTriplet,
+    HalfTriplet,
+}
+
+enum Accidental {
+    Sharp,
+    Flat,
+    Natural,
+}
+
 struct Line {
     clef_type: StaffType,
     line_height: usize,
     center_line: usize,
-    contents: Vec<String>,
+    contents: Vec<Bar>,
 }
+
+struct Bar {
+    pitches: Vec<Note>,
+    durations: Vec<Beats>,
+    measure_number: usize,
+}
+
+struct Note {
+    acidental: Accidental,
+    note_name: String,
+    octave: usize,
+}
+
 
 fn main() {
     println!("Hello, world!");
@@ -39,7 +83,7 @@ fn get_file(file_path: String) -> Result<Vec<String>, &'static str> {
  * This function takes the lines of code found in filepath and chunks them into
  * lines of music.
  */
-fn get_lines(code_lines: Vec<String>) -> Vec<Vec<String>> {
+fn get_raw_lines(code_lines: Vec<String>) -> Vec<Vec<String>> {
     let mut music_lines: Vec<Vec<String>> = Vec::new();
     let re = Regex::new(r"^=+$").unwrap();
 
@@ -58,11 +102,28 @@ fn get_lines(code_lines: Vec<String>) -> Vec<Vec<String>> {
     music_lines
 }
 
+
+fn get_tokenized_lines(lines: Vec<Vec<String>>) -> Result<Vec<Line>, ParsingError> {
+    for (line_number, line) in lines.iter().enumerate() {
+        let clef_type = match get_clef_type(line, line_number) {
+            Ok(staff) => staff,
+            Err(e) => return Err(e),
+        };
+
+        let center_index = get_center_line(line, clef_type);
+        //Break into bars please
+    }
+
+    Ok(Vec::new())
+}
+
+
 /*
  * This function takes a group of Strings that is one line of music (aka several
  * bars)
  */
-fn get_clef_type(line: Vec<String>) -> StaffType {
+fn get_clef_type(line: &Vec<String>, line_number: usize) -> Result<StaffType, ParsingError> {
+    // Get find the top regex and then make sure that they are all in order
     todo!()
 }
 
@@ -70,17 +131,19 @@ fn get_clef_type(line: Vec<String>) -> StaffType {
  * This function takes a group of strings that is one line of music and returns
  * the index of the center line (the note B4 for treble and D3 for bass)
  */
-fn get_center_line(line: Vec<String>, clef_type: StaffType) -> Result<usize, &'static str> {
+fn get_center_line(line: &Vec<String>, clef_type: StaffType) -> usize {
     let center_finder = match clef_type {
         StaffType::Bass => Regex::new(r"^\ {3}\/\ {4}l"),
         StaffType::Treble => Regex::new(r"^\ \/\ \|\ {3}l"),
     }.unwrap();
 
+    let mut center_index = 0;
     for (idx, row) in line.iter().enumerate() {
         if center_finder.is_match(row) {
-            return Ok(idx);
+            center_index = idx;
+            break;
         }
     }
 
-    Err("Center not found, invalid line was passed")
+    center_index
 }
